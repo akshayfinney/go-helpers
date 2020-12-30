ff76b74989c030069912ccdd644cd81fdf35466d
Akshay Finney <akshayfinney@Akshays-MacBook-Pro.local>
2020-12-30 13:36:23 -0800

    added docker pull with basic auth

---
+++ docker_auth_pull.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+)
+
+func main() {
+	fmt.Println("Enter Registry username && Password")
+	if len(os.Args) != 3 {
+		fmt.Println("Credentials required...Exiting")
+	}
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err)
+	}
+	authConfig := types.AuthConfig{
+		Username: os.Args[1],
+		Password: os.Args[2],
+	}
+	encodedJSON, err := json.Marshal(authConfig)
+	if err != nil {
+		panic(err)
+	}
+	authStr := base64.URLEncoding.EncodeToString(encodedJSON)
+
+	out, err := cli.ImagePull(ctx, "alpine", types.ImagePullOptions{RegistryAuth: authStr})
+	if err != nil {
+		panic(err)
+	}
+
+	defer out.Close()
+	io.Copy(os.Stdout, out)
+
+}
